c848618eaed8e0afc6c52ad500244eb1ad2b65c6
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-05-09 19:11:04 +0545

    funtion in js

---
+++ javascript/function/function.js
@@ -0,0 +1,13 @@
+//  A function is a block of reusable code that performs a specific task.
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("John");
+greet("Sara");
+
+
+function add(a,b){
+    console.log("addition",a+b);
+}
+add(3,2)
