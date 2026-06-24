0bc05e6077e176543f5dbee6b117d0ca2082a32f
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-06-24 08:04:20 +0545

    q1 and q2

---
+++ 5th sem Lab Reports/php/q1.php
@@ -0,0 +1,21 @@
+<?php
+// Variable Declaration
+$name = "Krishna";
+$age = 22;
+$college = "TU";
+$percentage = 85.5;
+
+// Output using echo
+echo "Student Information (Using echo)\n";
+echo "Name: $name\n";
+echo "Age: $age\n";
+echo "College: $college\n";
+echo "Percentage: $percentage%\n\n";
+
+// Output using print
+print "Student Information (Using print)\n";
+print "Name: $name\n";
+print "Age: $age\n";
+print "College: $college\n";
+print "Percentage: $percentage%\n";
+?>
